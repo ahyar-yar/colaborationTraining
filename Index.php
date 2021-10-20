d1bf1621f912dcf4de1f368aa2280aa62ba39dbd
Atikahdyah <tikahdyah@gmail.com>
2021-10-20 20:07:32 +0700

    mengedit file index.php

--- Index.php
+++ Index.php
@@ -4,6 +4,6 @@ echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 
-echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
+echo "<h3>Ng. Ahyar (205410093)</h3>"
 
 ?>
